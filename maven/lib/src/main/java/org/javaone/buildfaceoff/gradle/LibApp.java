f2091426b2d44420fdce898a5fb6d3c524d298e3
Oleg Shelajev <shelajev@gmail.com>
2017-03-23 12:40:59 -0700

    Final working build

--- maven/lib/src/main/java/org/javaone/buildfaceoff/gradle/LibApp.java
+++ maven/lib/src/main/java/org/javaone/buildfaceoff/gradle/LibApp.java
@@ -1,13 +1,11 @@
 package org.javaone.buildfaceoff.gradle;
 
-import lombok.Data;
 import org.javaone.buildfaceoff.AppC;
 
 /**
  * @author jbaruch
  * @since 9/16/16
  */
-@Data
 public class LibApp {
 
     public String getLibC(){
